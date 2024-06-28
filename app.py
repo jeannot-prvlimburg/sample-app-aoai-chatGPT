fd3bc4c3ae4196270f372859b6c57ab142e9b5ab
jeannot-prvlimburg <150451784+jeannot-prvlimburg@users.noreply.github.com>
2024-06-28 14:14:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -508,7 +508,7 @@ async def set_knowledge_base():
         new_settings.datasource.set_authentication()
 
         # Update the global app_settings
-        # global app_settings
+        global app_settings
         # app_settings = new_settings
 
         return jsonify({"success": True, "message": f"Knowledge base updated to {new_knowledge_base}"}), 200
