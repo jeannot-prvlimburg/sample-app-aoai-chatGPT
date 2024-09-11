c074c0b7423eb63c33fab2433079f12ca8f9181c
jeannot-github <62134006+jeannot-github@users.noreply.github.com>
2024-09-11 15:50:18 +0200

    Test 14

--- frontend/src/constants/KnowledgeBases.py
+++ frontend/src/constants/KnowledgeBases.py
@@ -5,7 +5,7 @@ KnowledgeBases = [
     },
     {
         "key": "stikstof",
-        "text": "Stikstof13",
+        "text": "Stikstof14",
         "endpoint": "stikstof-v2.azurewebsites.net",
         "api_key": "fnwvwCuSUfVpx2p9R4lPb6S8y2W8RqvyZhqNwSOxDJAzSeDAnSBi",
         "index_name": "stikstof-24042024",
