30c5dec09ab36702eced3928e44b30bd1455a303
jeannot-github <62134006+jeannot-github@users.noreply.github.com>
2024-09-13 14:47:38 +0200

    small iteration

--- app.py
+++ app.py
@@ -29,10 +29,9 @@ from backend.history.cosmosdbservice import CosmosConversationClient
 from backend.settings import MINIMUM_SUPPORTED_AZURE_OPENAI_PREVIEW_API_VERSION
 from backend.settings import app_settings
 
-try:
-    from backend.settings import create_user_app_settings
-except:
-    pass
+
+from backend.settings import create_user_app_settings
+
 
 from backend.utils import (
     format_as_ndjson,
